2f3142d065b200b29ab13e8557ba555e07224555
raghu mudgal <rmudgal23@gmail.com>
2020-01-16 17:53:01 +0530

    "Type Ahead look up Serach box",issue resolving,new implementation

---
+++ TransportService.Web/Controllers/GoogleMap1Controller.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TransportService.Web.Controllers
+{
+    public class GoogleMap1Controller : Controller
+    {
+        // GET: GoogleMap1
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Distance()
+        {
+            return View();
+        }
+
+    }
+}
